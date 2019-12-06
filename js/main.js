a7207780b42ae5bf144a82430171e64cd3879dff
bonbonvision <csr7@morriscostumes.com>
2019-12-06 16:42:41 -0500

    Added test for photos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,66 @@
-$('#file-upload').change(function() {
-  var i = $(this).prev('label').clone();
-  var file = $('#file-upload')[0].files[0].name;
-  $(this).prev('label').text(file);
+var loadFile = function (event) {
+  var image = document.getElementById('outputPhoto1')
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+// 2nd Test
+
+$(function () {
+  var imagesPreview = function (input, placeToInsertImagePreview) {
+
+    if (input.files) {
+      var filesAmount = input.files.length;
+      for (i = 0; i < filesAmount; i++) {
+        var reader = new FileReader();
+        reader.onload = function (event) {
+          $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+        }
+        reader.readAsDataURL(input.files[i]);
+      }
+    }
+  };
+
+  $('#gallery-photo-add').on('change', function () {
+    imagesPreview(this, 'div.gallery');
+  });
 });
+
+// 3rd Test
+
+window.URL = window.URL || window.webkitURL;
+
+const fileSelect = document.getElementById("fileSelect"),
+  fileElem = document.getElementById("fileElem"),
+  fileList = document.getElementById("fileList");
+
+fileSelect.addEventListener("click", function (e) {
+  if (fileElem) {
+    fileElem.click();
+  }
+  e.preventDefault();
+}, false);
+
+function handleFiles(files) {
+  if (!files.length) {
+    fileList.innerHTML = "<p>No files selected!</p>";
+  } else {
+    fileList.innerHTML = "";
+    const list = document.createElement("ul");
+    fileList.appendChild(list);
+    for (let i = 0; i < files.length; i++) {
+      const li = document.createElement("li");
+      list.appendChild(li);
+
+      const img = document.createElement("img");
+      img.src = window.URL.createObjectURL(files[i]);
+      img.height = 60;
+      // img.onload = function() {
+      //   window.URL.revokeObjectURL(this.src);
+      // }
+      li.appendChild(img);
+      const info = document.createElement("span");
+      info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
+      li.appendChild(info);
+    }
+  }
+}
